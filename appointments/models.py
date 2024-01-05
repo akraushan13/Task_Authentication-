7cbcb57109c69250a29a6a040edc795b1f5112ad
Avinash Kumar <akraushan13@gmail.com>
2024-01-05 20:49:53 +0530

    Add end_time

--- appointments/models.py
+++ appointments/models.py
@@ -2,11 +2,21 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.shortcuts import reverse
 
+from datetime import date, datetime, timedelta
 
 class Appointment(models.Model):
     speciality = models.CharField(max_length=255)
     date = models.DateField()
     time = models.TimeField()
+    end_time = models.TimeField(null=True, blank=True)
+    
+    def save(self, *args, **kwargs):
+        # Add 45 minutes to start_time to calculate end_time
+        if self.time and not self.end_time:
+            end_time = (datetime.combine(date.today(), self.time) + timedelta(minutes=45)).time()
+            self.end_time = end_time
+        super().save(*args, **kwargs)
+    
     patient = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, related_name="doctor_appointed"
     )
